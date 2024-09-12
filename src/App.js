cd0a176299637e1175f5c8f8d8ac5164d8020ab2
Olia <ola.talash@gmail.com>
2024-09-12 19:56:48 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 
 function App() {
   return (
     <div className="App">
-      
+      <RouterProvider router={router} />
     </div>
   );
 }
