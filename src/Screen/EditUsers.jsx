6e8c6901ed2af5217f9353cd5d028a4c00ab6ab9
Olia <ola.talash@gmail.com>
2024-09-16 19:32:44 +0300

    create full name

--- src/Screen/EditUsers.jsx
+++ src/Screen/EditUsers.jsx
@@ -15,26 +15,43 @@ export default function EditUsers(){
 
 function EditUserForm(){
     const [user, setUser] = useState(null);
+    const [fullName, setFullName] = useState('');
+    const userUpdate = (user) => {
+        setUser(user);
+        setFullName(user.name);
+    };
+
 
     return <div className="edit_user_form">
-                <h1>Edit Users</h1>
-                <div className="user_name">
-                    <label>
-                        User
-                        <Select 
-                            value={user} 
-                            onChange={setUser} 
-                            options={users} 
-                            getOptionLabel={user=>user.name} 
-                            getOptionValue={user => user.name} 
-                            placeholder={'Oleg Schevchenko'}
-                        />
-                    </label>
+                <div className="edit_user_form_wrap">
+                    <h2>Edit Users</h2>
+                        <label>
+                            User
+                            <div className="user_name">
+                                
+                            <Select 
+                                value={user} 
+                                onChange={userUpdate} 
+                                options={users} 
+                                getOptionLabel={user=>user.name} 
+                                getOptionValue={user => user.name} 
+                                placeholder={'Oleg Schevchenko'}
+                            />
+                            </div>
+                        </label>
 
-                    {user &&
-                        <div className="form">
-                            <h4>User Information</h4>
-                        </div> }
+                        {user &&
+                            <div className="form">
+                                <h3>User Information</h3>
+                                <label >
+                                    Full Name
+                                    <input 
+                                        onChange={e => (setFullName(e.target.value))} 
+                                        type="text" 
+                                        value={fullName} 
+                                    />
+                                </label>
+                            </div> }
                 </div>
             </div>
 }
